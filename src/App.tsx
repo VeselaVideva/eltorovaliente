86eaf7fee59f0af1227642ca9234f08d1d8e3b9d
Vesela Videva <vesela.videva@gmail.com>
2023-07-17 23:18:08 +0300

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <p>Under construction...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
