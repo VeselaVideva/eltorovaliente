631525a80231e5f2fb88ffeb78c7a3a29544a4be
Vesela Videva <vesela.videva@gmail.com>
2023-07-18 14:50:48 +0300

    Add header and footer

---
+++ src/components/Header.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+import { createStyles, Header, Group, Container, Burger, rem } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { ThemeToggle } from "./ThemeSwitch";
+
+const useStyles = createStyles((theme) => ({
+    inner: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: rem(56),
+
+        [theme.fn.smallerThan('sm')]: {
+            justifyContent: 'flex-start',
+        },
+    },
+
+    links: {
+        width: rem(260),
+
+        [theme.fn.smallerThan('sm')]: {
+            display: 'none',
+        },
+    },
+
+    social: {
+        width: rem(260),
+
+        [theme.fn.smallerThan('sm')]: {
+            width: 'auto',
+            marginLeft: 'auto',
+        },
+    },
+
+    burger: {
+        marginRight: theme.spacing.md,
+
+        [theme.fn.largerThan('sm')]: {
+            display: 'none',
+        },
+    },
+
+    link: {
+        display: 'block',
+        lineHeight: 1,
+        padding: `${rem(8)} ${rem(12)}`,
+        borderRadius: theme.radius.sm,
+        textDecoration: 'none',
+        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+        fontSize: theme.fontSizes.sm,
+        fontWeight: 500,
+
+        '&:hover': {
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        },
+    },
+
+    linkActive: {
+        '&, &:hover': {
+            backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
+            color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+        },
+    },
+}));
+
+interface HeaderMiddleProps {
+    links: { link: string, label: string }[];
+}
+
+export function HeaderMiddle({ links }: HeaderMiddleProps) {
+    const [opened, { toggle }] = useDisclosure(false);
+    const [active, setActive] = useState('');
+    const { classes, cx } = useStyles();
+
+    const items = links.map((link) => (
+        <a
+            key={link.label}
+            href={link.link}
+            className={cx(classes.link, { [classes.linkActive]: active === link.link })}
+            onClick={(event) => {
+                event.preventDefault();
+                setActive(link.link);
+            }}
+        >
+            {link.label}
+        </a>
+    ));
+
+    return (
+        <Header height={56} mb={120}>
+            <Container className={classes.inner}>
+                <Burger opened={opened} onClick={toggle} size="sm" className={classes.burger} />
+                <Group className={classes.links} spacing={5}>
+                    {items}
+                </Group>
+                <p>EL TORO VALIENTE</p>
+                <Group spacing={0} className={classes.social} position="right" noWrap>
+                    <ThemeToggle />
+                </Group>
+            </Container>
+        </Header>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { AppWithColorScheme } from "./components/AppWithColorScheme";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <AppWithColorScheme/>
+    </React.StrictMode>
 );
