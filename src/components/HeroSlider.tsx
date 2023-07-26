ac9684a6620bc6fbf86560f68cfa074d2e1d750b
Vesela Videva <vesela.videva@gmail.com>
2023-07-26 11:56:33 +0300

    Create carousel with images

---
+++ src/components/HeroSlider.tsx
@@ -0,0 +1,14 @@
+import { Carousel } from '@mantine/carousel';
+import { Container, Image } from "@mantine/core";
+
+export function HeroSlider() {
+    return (
+        <Container size="xl">
+            <Carousel slideSize="100%" height="70vh" slideGap="xl" controlsOffset="md" loop withIndicators>
+                <Carousel.Slide><Image src="http://eltorovaliente.com/images/bg.jpg"/></Carousel.Slide>
+                <Carousel.Slide><Image src="http://eltorovaliente.com/images/bg.jpg"/></Carousel.Slide>
+                <Carousel.Slide><Image src="http://eltorovaliente.com/images/bg.jpg"/></Carousel.Slide>
+            </Carousel>
+        </Container>
+    );
+}
