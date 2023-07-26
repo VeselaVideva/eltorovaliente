7bd301f092e4d4f32c40b9f2cf325c3e44fec863
Vesela Videva <vesela.videva@gmail.com>
2023-07-26 11:56:01 +0300

    Create shows sections

--- src/components/Shows.tsx
+++ src/components/Shows.tsx
@@ -1,33 +1,40 @@
-import { Timeline, Text, Container, Grid, Space } from '@mantine/core';
+import React from "react";
+import { Timeline, Text, Container, Grid, Space, Anchor } from '@mantine/core';
 import { IconGitBranch } from '@tabler/icons-react';
 
-export function ShowsTimeline() {
+export function Shows() {
     return (
-        <Container my="md">
+        <Container size="xl">
+            <Text id="shows" fz="xl" align="center" className="section-title">
+                Shows
+                <Anchor href="#shows" aria-hidden="true"></Anchor>
+            </Text>
+            <Space h="xl" />
+            <Space h="xl" />
             <Grid>
-                <Grid.Col span={6} className="timeline-left-column">
-                    <Timeline active={-1} bulletSize={36} lineWidth={1} align="right">
-                        <Timeline.Item bullet={<IconGitBranch size={18}/>} title="2021.02.18">
-                            <Text color="dimmed" size="md">Puppy class</Text>
-                            <Space h="xl" />
-                            <Space h="xl" />
+                <Grid.Col span={4} offset={2}>
+                    <Timeline active={-1} bulletSize={36} lineWidth={1} align="left">
+                        <Timeline.Item bullet={<IconGitBranch size={18}/>} title="Sample title">
+                            <Text color="dimmed" size="md">You&apos;ve created new branch from master</Text>
+                            <Text color="dimmed" size="md">You&apos;ve created new branch from master</Text>
+                            <Text color="dimmed" size="md">You&apos;ve created new branch from master</Text>
 
                         </Timeline.Item>
-                        <Timeline.Item bullet={<IconGitBranch size={18}/>} title="2021.02.18">
-                            <Text color="dimmed" size="md">Puppy class</Text>
-                            <Space h="xl" />
-                            <Space h="xl" />
+                        <Timeline.Item bullet={<IconGitBranch size={18}/>} title="Sample title">
+                            <Text color="dimmed" size="md">You&apos;ve created new branch from master</Text>
+                            <Text color="dimmed" size="md">You&apos;ve created new branch from master</Text>
+                            <Text color="dimmed" size="md">You&apos;ve created new branch from master</Text>
 
                         </Timeline.Item>
-                        <Timeline.Item bullet={<IconGitBranch size={18}/>} title="2021.02.18">
-                            <Text color="dimmed" size="md">Puppy class</Text>
-                            <Space h="xl" />
-                            <Space h="xl" />
-                            {/*<Text size="sm" mt={4}>Puppy class</Text>*/}
+                        <Timeline.Item bullet={<IconGitBranch size={18}/>} title="Sample title">
+                            <Text color="dimmed" size="md">You&apos;ve created new branch from master</Text>
+                            <Text color="dimmed" size="md">You&apos;ve created new branch from master</Text>
+                            <Text color="dimmed" size="md">You&apos;ve created new branch from master</Text>
+                            {/*<Text size="sm" mt={4}>2 hours ago</Text>*/}
                         </Timeline.Item>
                     </Timeline>
                 </Grid.Col>
-                <Grid.Col span={6} className="timeline-right-column">
+                <Grid.Col span={4} offset={1}>
                     <Timeline active={-1} bulletSize={36} lineWidth={1} align="left">
                         <Timeline.Item bullet={<IconGitBranch size={18}/>} title="Sample title">
                             <Text color="dimmed" size="md">You&apos;ve created new branch from master</Text>
