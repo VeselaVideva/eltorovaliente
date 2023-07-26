9259d28f0398ffee4d5086cd104e280777d231e2
Vesela Videva <vesela.videva@gmail.com>
2023-07-26 11:55:28 +0300

    Add anchors and smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import './App.css';
 import { HeaderMiddle } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Main } from "./components/Main";
+import { Anchor, Text } from "@mantine/core";
 
 function App() {
     return (
         <div className="App">
+            <Text id="gototop">
+                <Anchor href="#gototop" aria-hidden="true"></Anchor>
+            </Text>
             <HeaderMiddle links={[
-                {link: '/pedigree', label: 'Pedigree'},
-                {link: '/shows', label: 'Shows'},
-                {link: '/health', label: 'Health'}
+                { link: '#pedigree', label: 'Pedigree' },
+                { link: '#shows', label: 'Shows' },
+                { link: '#health', label: 'Health' }
             ]}/>
             <Main/>
             <Footer/>
