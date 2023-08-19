0abccfc456ac8b6d88b7d87481668f196a0d8b30
Vesela Videva <vesela.videva@gmail.com>
2023-08-19 16:25:01 +0300

    Upload build folder

---
+++ build/precache-manifest.502ec3a85a4f3ff6bc3cebef0dafa687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f3e326ca3e18d9ee288",
+    "url": "/static/css/main.fc3b409d.chunk.css"
+  },
+  {
+    "revision": "9f3e326ca3e18d9ee288",
+    "url": "/static/js/main.9f3e326c.chunk.js"
+  },
+  {
+    "revision": "082be550d0607d8ca5f2",
+    "url": "/static/js/1.082be550.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "02142abd980fd2919250293ef7df390c",
+    "url": "/index.html"
+  }
+];
