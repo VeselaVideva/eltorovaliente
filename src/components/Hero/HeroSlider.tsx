37d0927bbd625c1197374f8752c1ea1d75c9f736
Vesela Videva <vesela.videva@gmail.com>
2023-08-19 14:11:09 +0300

    Update carousel

--- src/components/Hero/HeroSlider.tsx
+++ src/components/Hero/HeroSlider.tsx
@@ -4,11 +4,24 @@ import { Container, Image } from "@mantine/core";
 
 export function HeroSlider() {
     return (
-        <Container size="xl">
-            <Carousel slideSize="100%" height="70vh" slideGap="xl" controlsOffset="xl" loop withIndicators>
-                <Carousel.Slide><Image src="http://eltorovaliente.com/images/bg.jpg"/></Carousel.Slide>
-                <Carousel.Slide><Image src="http://eltorovaliente.com/images/bg.jpg"/></Carousel.Slide>
-                <Carousel.Slide><Image src="http://eltorovaliente.com/images/bg.jpg"/></Carousel.Slide>
+        <Container size="xl" style={{ height: 'auto', display: 'flex' }}>
+            <Carousel
+                sx={{ flex: 1 }}
+                height="100%"
+                slideSize="100%"
+                slideGap="xl"
+                withIndicators
+                loop
+                breakpoints={[
+                    { maxWidth: 'md', slideSize: '100%' },
+                    { maxWidth: 'sm', slideSize: '100%', slideGap: "xs" },
+                ]}
+            >
+                <Carousel.Slide><Image src="/images/bg_01.jpg"/></Carousel.Slide>
+                <Carousel.Slide><Image src="/images/bg_02.jpg"/></Carousel.Slide>
+                <Carousel.Slide><Image src="/images/bg_03.jpg"/></Carousel.Slide>
+                <Carousel.Slide><Image src="/images/bg_04.jpg"/></Carousel.Slide>
+                <Carousel.Slide><Image src="/images/bg_05.jpg"/></Carousel.Slide>
             </Carousel>
         </Container>
     );
