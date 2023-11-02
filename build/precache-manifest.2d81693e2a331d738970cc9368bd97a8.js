09337e3bb0dde323011c586bf12da944a1ad3bc1
Vesela Videva <vesela.videva@gmail.com>
2023-11-02 12:55:39 +0200

    Build

---
+++ build/precache-manifest.2d81693e2a331d738970cc9368bd97a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b1733f9351886e795b8",
+    "url": "/static/css/main.fc3b409d.chunk.css"
+  },
+  {
+    "revision": "9b1733f9351886e795b8",
+    "url": "/static/js/main.9b1733f9.chunk.js"
+  },
+  {
+    "revision": "082be550d0607d8ca5f2",
+    "url": "/static/js/1.082be550.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a0cafc87c61392d978ca038859143db",
+    "url": "/index.html"
+  }
+];
