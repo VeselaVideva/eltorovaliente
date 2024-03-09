775b2bb013b8e84ddb1a9be549f9ae5901097712
Vesela Videva <vesela.videva@gmail.com>
2024-03-09 23:36:21 +0200

    Create new prod build

---
+++ build/precache-manifest.537c8faddbb7d0dabc9fabd364662ff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f734a1d188c750c941c",
+    "url": "/static/css/main.dd545bcf.chunk.css"
+  },
+  {
+    "revision": "2f734a1d188c750c941c",
+    "url": "/static/js/main.2f734a1d.chunk.js"
+  },
+  {
+    "revision": "0ed5412f0b3bd14e8185",
+    "url": "/static/js/1.0ed5412f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "772c1f2eb0b5ba069d84ef87005bdb58",
+    "url": "/index.html"
+  }
+];
