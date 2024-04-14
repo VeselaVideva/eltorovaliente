4fcc58fe786795b81e59d62b53c5f93d2c8c8dbe
Vesela Videva <vesela.videva@gmail.com>
2024-04-14 15:26:40 +0300

    Update build

---
+++ build/precache-manifest.afafaae913b68e2d24e35606ec674199.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69b138528239915239fa",
+    "url": "/static/css/main.f914824c.chunk.css"
+  },
+  {
+    "revision": "69b138528239915239fa",
+    "url": "/static/js/main.69b13852.chunk.js"
+  },
+  {
+    "revision": "0ed5412f0b3bd14e8185",
+    "url": "/static/js/1.0ed5412f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "612582a847310e10bd320385a4968a4e",
+    "url": "/index.html"
+  }
+];
